3894142fe4b2d16bcde189d724767291c62f87b5
JonMooseMejia <48665042+JonMooseMejia@users.noreply.github.com>
2021-02-04 22:35:09 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tryme</title>
+</head>
+<body>
+    <?php include "tryme.php";
+    ?>
+</body>
+</html>
